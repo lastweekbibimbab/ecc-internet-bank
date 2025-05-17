c9f4c92150c7cee4dd4985b166759617fd092414
lastweekbibimbab <doheoni66@gmail.com>
2025-05-17 14:06:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -160,4 +160,5 @@ def loan():
     return jsonify(msg="대출 완료")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
